712b6611a6dc1c9bc866aa83a0b6d3fd4bb3726f
Brian Takita <brian.takita@gmail.com>
2023-11-28 18:48:16 -0500

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://brookebrodack.me',
-	integrations: [sitemap(), mdx(), tailwind()]
+	integrations: [sitemap(), mdx(), tailwind()],
+	...(import.meta.env.BROOKEBRODACK_PORT ? {
+		server: {
+			host: '0.0.0.0', // heroku requires 0.0.0.0
+			port: parseInt(import.meta.env.BROOKEBRODACK_PORT)
+		}
+	} : {}),
 })
