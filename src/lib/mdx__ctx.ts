895c9b583ce4556c453838e13eda0f3d6ed88db6
Brian Takita <brian.takita@gmail.com>
2023-11-30 01:56:31 -0500

    package renames:
    	@btakita/domain--all--blog→@btakita/domain--any--blog
    	@btakita/ui--all--blog→@btakita/ui--any--blog
    	@btakita/ui--all--brookebrodack→@btakita/ui--any--brookebrodack

--- src/lib/mdx__ctx.ts
+++ src/lib/mdx__ctx.ts
@@ -1,4 +1,4 @@
-import { blog__ctx__new } from '@btakita/domain--all--blog'
+import { blog__ctx__new } from '@btakita/domain--any--blog'
 import { logo_image__set, site__set, socials__set } from '@btakita/domain--server--blog'
 import { LOGO_IMAGE, SITE, SOCIALS } from '@config'
 import { relement__use } from 'relementjs'
