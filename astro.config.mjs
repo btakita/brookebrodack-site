56ac4c0b794a35b1eb821944418840e98009c4d5
Brian Takita <brian.takita@gmail.com>
2023-12-01 04:10:49 -0500

    code:
    	tsconfig.json:
    		"moduleResolution": "nodenext"
    		- jsx
    	format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://brookebrodack.me',
