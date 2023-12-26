a61c07bb7feccf61ac361fc973f616ef956b80d1
Brian Takita <brian.takita@gmail.com>
2023-12-25 21:27:25 -0500

    + src/app/brookers middleware: extracted from src/app
    ∋ esmcss
    
    update dependencies

--- src/build.ts
+++ src/build.ts
@@ -1,6 +1,7 @@
 import { is_entry_file_ } from '@ctx-core/nodejs'
 import esbuild_style_plugin from 'esbuild-style-plugin'
-import { browser__build, cssjs_esbuild_plugin_, type relysjs__build_config_T, server__build } from 'relysjs'
+import { esmcss_esbuild_plugin_ } from 'esmcss'
+import { browser__build, type relysjs__build_config_T, server__build } from 'relysjs'
 import tailwind from 'tailwindcss'
 import { config__init } from './app/index.js'
 export async function build(config?:relysjs__build_config_T) {
@@ -18,7 +19,7 @@ export async function build(config?:relysjs__build_config_T) {
 			'.mp4': 'file',
 			'.svg': 'file',
 		},
-		plugins: [cssjs_esbuild_plugin_(), esbuild_style_plugin({
+		plugins: [esmcss_esbuild_plugin_(), esbuild_style_plugin({
 			postcss: {
 				plugins: [tailwind]
 			}
