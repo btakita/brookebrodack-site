902f9dee5611ec510a2575812346dbdf4a5066e7
Brian Takita <brian.takita@gmail.com>
2023-12-28 04:06:44 -0500

    app: format:
    	index.server.ts
    	brookers/index.server.ts
    
    ∋ @btakita/ui--server--brookebrodack

--- src/app/brookers/index.server.ts
+++ src/app/brookers/index.server.ts
@@ -1,10 +1,7 @@
 import '../index.css'
-import { brookers__page_ } from '@btakita/ui--server--brookebrodack'
+import { brookers__page_ } from '@btakita/ui--server--brookebrodack/brookers'
 import { type DecoratorBase, Elysia } from 'elysia'
 import { type elysia_context_T, html_route_, middleware_ } from 'relysjs'
-export default middleware_(middleware_ctx=>{
-	return new Elysia<'', DecoratorBase&elysia_context_T>({
-		name: 'root_routes'
-	})
-		.get('/brookers', html_route_(middleware_ctx, brookers__page_))
-})
+export default middleware_(middleware_ctx=>
+	new Elysia<'', DecoratorBase&elysia_context_T>({ name: 'brookers_routes' })
+		.get('/brookers', html_route_(middleware_ctx, brookers__page_)))
