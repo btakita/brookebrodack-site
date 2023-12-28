82a5969af5a6df8d15add97d2747f2bfed8337d6
Brian Takita <brian.takita@gmail.com>
2023-12-28 03:21:06 -0500

    app:
    	index.server.ts:
    	brookers/index.server.ts:
    		∋ html_route_
    
    build: tailwindcss: content: fix: tailwindcss is run against all output__relative_path_a for each cssBundle
    
    update dependencies

--- src/app/index.server.ts
+++ src/app/index.server.ts
@@ -1,31 +1,10 @@
 import './index.css'
-import { brookers__page_, home__page_ } from '@btakita/ui--server--brookebrodack'
+import { home__page_ } from '@btakita/ui--server--brookebrodack'
 import { type DecoratorBase, Elysia } from 'elysia'
-import type { elysia_context_T } from 'relysjs'
-import { elysia_context__set, middleware_, route_ctx_, type route_ctx_T } from 'relysjs'
+import { type elysia_context_T, html_route_, middleware_ } from 'relysjs'
 export default middleware_(middleware_ctx=>{
 	return new Elysia<'', DecoratorBase&elysia_context_T>({
 		name: 'root_routes'
 	})
-		.get('/', route_(home__page_))
-		.get('/brookers', route_(brookers__page_))
-	function route_(page_:($p:{ ctx:route_ctx_T })=>string) {
-		return (context:elysia_context_T)=>{
-			const ctx = route_ctx_(middleware_ctx)
-			elysia_context__set(ctx, context)
-			return new Response(
-				new ReadableStream({
-					start(controller) {
-						controller.enqueue(page_({ ctx }))
-						controller.close()
-					}
-				}),
-				{
-					headers: {
-						'Content-Type': 'text/html;charset=UTF-8',
-					}
-				}
-			)
-		}
-	}
+		.get('/', html_route_(middleware_ctx, home__page_))
 })
