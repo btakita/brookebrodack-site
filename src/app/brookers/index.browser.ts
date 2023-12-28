94f6c98287b4b3469633713fce29dc27640e38e5
Brian Takita <brian.takita@gmail.com>
2023-12-28 04:06:44 -0500

    app: format:
    	index.server.ts
    	brookers/index.server.ts
    
    ∋ @btakita/ui--server--brookebrodack

--- src/app/brookers/index.browser.ts
+++ src/app/brookers/index.browser.ts
@@ -1,5 +1,5 @@
-import { brookers__page__hy__bind__id } from '@btakita/ui--any--brookebrodack'
-import { brookers__page__hy__bind } from '@btakita/ui--browser--brookebrodack'
+import { brookers__page__hy__bind__id } from '@btakita/ui--any--brookebrodack/brookers'
+import { brookers__page__hy__bind } from '@btakita/ui--browser--brookebrodack/brookers'
 import { hy__bind } from 'relementjs/browser'
 window.addEventListener('load', ()=>{
 	queueMicrotask(()=>{
--- src/app/index.server.ts
+++ src/app/index.server.ts
@@ -1,10 +1,7 @@
 import './index.css'
-import { home__page_ } from '@btakita/ui--server--brookebrodack'
+import { home__page_ } from '@btakita/ui--server--brookebrodack/home'
 import { type DecoratorBase, Elysia } from 'elysia'
 import { type elysia_context_T, html_route_, middleware_ } from 'relysjs'
-export default middleware_(middleware_ctx=>{
-	return new Elysia<'', DecoratorBase&elysia_context_T>({
-		name: 'root_routes'
-	})
-		.get('/', html_route_(middleware_ctx, home__page_))
-})
+export default middleware_(middleware_ctx=>
+	new Elysia<'', DecoratorBase&elysia_context_T>({ name: 'root_routes' })
+		.get('/', html_route_(middleware_ctx, home__page_)))
