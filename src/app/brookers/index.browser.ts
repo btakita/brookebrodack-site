5e797ed281647fe433a505e30c43221728dac9bb
Brian Takita <brian.takita@gmail.com>
2024-01-14 23:37:49 -0500

    update dependencies

--- src/app/brookers/index.browser.ts
+++ src/app/brookers/index.browser.ts
@@ -1,10 +1,10 @@
 import { brookers__page__hy__bind__id } from '@btakita/ui--any--brookebrodack/brookers'
-import { brookers__page__hy__bind } from '@btakita/ui--browser--brookebrodack/brookers'
+import { brookers__page__bind } from '@btakita/ui--browser--brookebrodack/brookers'
 import { hy__bind } from 'relementjs/browser'
 window.addEventListener('load', ()=>{
 	queueMicrotask(()=>{
 		hy__bind(document, {
-			[brookers__page__hy__bind__id]: brookers__page__hy__bind,
+			[brookers__page__hy__bind__id]: brookers__page__bind,
 		})
 	})
 })
--- start.ts
+++ start.ts
@@ -3,4 +3,4 @@ await import(
 	process.env.NODE_ENV === 'production'
 		? './dist/server/index.js'
 		: './dist/dev-server/index.js'
-	).then(mod=>mod.default())
+).then(mod=>mod.default())
