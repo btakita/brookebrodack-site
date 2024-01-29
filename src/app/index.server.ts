f92968bc4d5b30a1795e6032dbd193843fcbc6f4
Brian Takita <brian.takita@gmail.com>
2024-01-28 23:43:49 -0500

    /: index.server.ts: format
    tsconfig.json: remove astro reference

--- src/app/index.server.ts
+++ src/app/index.server.ts
@@ -3,5 +3,6 @@ import { home__page_ } from '@btakita/ui--server--brookebrodack/home'
 import { type DecoratorBase, Elysia } from 'elysia'
 import { type elysia_context_T, html_route_, middleware_ } from 'relysjs/server'
 export default middleware_(middleware_ctx=>
-	new Elysia<'', DecoratorBase&elysia_context_T>({ name: 'root_routes' })
-		.get('/', html_route_(middleware_ctx, home__page_)))
+	new Elysia<'', DecoratorBase&elysia_context_T>({
+		name: 'root_routes'
+	}).get('/', html_route_(middleware_ctx, home__page_)))
