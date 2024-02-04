faacc2321b7df48d1db38cb98074c1e2c4248c8f
Brian Takita <brian.takita@gmail.com>
2024-02-03 19:39:38 -0500

    /brookers: hy__bind: ∋ brookers__doc_html__bind→brookers__div__bind

--- src/app/brookers/index.browser.ts
+++ src/app/brookers/index.browser.ts
@@ -1,9 +1,9 @@
-import { brookers__doc_html__bind } from '@btakita/ui--browser--brookebrodack/brookers'
+import { brookers__div__bind } from '@btakita/ui--browser--brookebrodack/brookers'
 import { hy__bind } from 'relementjs/browser'
 window.addEventListener('load', ()=>{
 	queueMicrotask(()=>{
 		hy__bind(document, {
-			brookers: brookers__doc_html__bind,
+			brookers: brookers__div__bind,
 		})
 	})
 })
