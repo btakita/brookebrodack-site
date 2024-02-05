1b5664ed584fffbea2453d935618e308683a9cd0
Brian Takita <brian.takita@gmail.com>
2024-02-05 18:27:24 -0500

    src/app/index.ts: - prebuild
    update dependencies

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,23 +4,21 @@ import { dirname, join, resolve } from 'path'
 import { relement__use } from 'relementjs'
 import { server__relement } from 'relementjs/server'
 import { app_ctx, compression_middleware_, cwd__set, is_prod_, port__set, static_middleware_ } from 'relysjs/server'
-export function prebuild() {
+export default async ()=>{
+	config__init()
 	return new Elysia()
+		.use(await static_middleware_(
+			is_prod_(app_ctx)
+				? {
+					headers_: ()=>({
+						'Cache-Control': 'max-age=2592000, public'
+					})
+				}
+				: {}))
 		.use(compression_middleware_())
 		.onError(({ error, request })=>{
 			console.error(request.url, error)
 		})
-}
-export default async ()=>{
-	config__init()
-	return static_middleware_(
-		is_prod_(app_ctx)
-			? {
-				headers_: ()=>({
-					'Cache-Control': 'max-age=2592000, public'
-				})
-			}
-			: {})
 }
 export function config__init() {
 	const port = parseInt(import_meta_env_().BROOKEBRODACK_PORT) || 4101
--- start.ts
+++ start.ts
@@ -1,10 +1,7 @@
-import { Elysia } from 'elysia'
 import { app__start } from 'relysjs/server'
 const mod = await import(
 	process.env.NODE_ENV === 'production'
 		? './dist/server/index.js'
 		: './dist/server--dev/index.js'
 )
-await app__start(
-	(await mod.prebuild() ?? new Elysia())
-		.use(await mod.default()))
+await app__start(await mod.default())
