1d92c39b6f8c3a3744b7fa222994c919099a288f
Brian Takita <brian.takita@gmail.com>
2024-02-06 22:37:48 -0500

    /: remove browser js
    fix: /brookers

--- src/app/index.browser.ts
+++
@@ -1,3 +0,0 @@
-import { relement__use } from 'relementjs'
-import { browser__relement } from 'relementjs/browser'
-relement__use(browser__relement)
--- src/app/store/index.server.ts
+++ src/app/store/index.server.ts
@@ -3,7 +3,7 @@ import { type DecoratorBase, Elysia } from 'elysia'
 import { type elysia_context_T, middleware_ } from 'relysjs/server'
 export default middleware_(middleware_ctx=>
 	new Elysia<'', DecoratorBase&elysia_context_T>({
-		name: 'brookers_routes'
+		name: 'store_routes'
 	}).get(
-		'/brookers',
+		'/store',
 		()=>'<div class="mt-0"></div>'))
