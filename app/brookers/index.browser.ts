1279110f90124e1cfdede0d3806bf3a0d6a5b641
Brian Takita <brian.takita@gmail.com>
2024-02-11 21:58:11 -0500

    /brookers: browser: brookers→brookers__hyop

--- app/brookers/index.browser.ts
+++ app/brookers/index.browser.ts
@@ -1,9 +1,9 @@
-import { brookers__div__bind } from '@btakita/ui--browser--brookebrodack/brookers'
+import { brookers__hyop } from '@btakita/ui--browser--brookebrodack/brookers'
 import { single_hyop } from 'relementjs/browser/hy'
 window.addEventListener('load', ()=>{
 	queueMicrotask(()=>{
 		single_hyop(document, {
-			brookers: brookers__div__bind,
+			brookers__hyop,
 		})
 	})
 })
