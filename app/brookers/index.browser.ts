179c85eb26e05ce5a3c90d4f49797002f70906e9
Brian Takita <brian.takita@gmail.com>
2024-02-13 08:35:26 -0500

    /brookers: browser: ∋ YT_iframe__div__hyop

--- app/brookers/index.browser.ts
+++ app/brookers/index.browser.ts
@@ -1,9 +1,11 @@
 import { brookers__hyop } from '@btakita/ui--browser--brookebrodack/brookers'
+import { YT_iframe__div__hyop } from '@btakita/ui--browser--brookebrodack/youtube'
 import { single_hyop } from 'relementjs/browser/hy'
 window.addEventListener('load', ()=>{
 	queueMicrotask(()=>{
 		single_hyop(document, {
 			brookers__hyop,
+			YT_iframe__div__hyop,
 		})
 	})
 })
