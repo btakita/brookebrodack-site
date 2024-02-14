4db625b2b7c4e5e826ae387159e78a2bc225fd4f
Brian Takita <brian.takita@gmail.com>
2024-02-13 21:59:27 -0500

    /brookers: browser:
    	- ∋ queueMicrotask
    	+ ∋ spinner__template__hyop
    
    /content: + browser:
    
    	+ ∋ single_hyop
    		+ ∋ content__hyop
    		+ ∋ spinner__template__hyop
    		+ ∋ YT_player__div__hyop
    
    update dependencies

--- app/content/index.browser.ts
+++ app/content/index.browser.ts
@@ -1,10 +1,10 @@
-import { brookers__hyop } from '@btakita/ui--browser--brookebrodack/brookers'
+import { content__hyop } from '@btakita/ui--browser--brookebrodack/content'
 import { spinner__template__hyop } from '@btakita/ui--browser--brookebrodack/spinner'
 import { YT_player__div__hyop } from '@btakita/ui--browser--brookebrodack/youtube'
 import { single_hyop } from 'relementjs/browser/hy'
 window.addEventListener('load', ()=>{
 	single_hyop(document, {
-		brookers__hyop,
+		content__hyop,
 		spinner__template__hyop,
 		YT_player__div__hyop,
 	})
