c8ca5e11ceef71801d7ed8d05c6907ba7bff166b
Brian Takita <brian.takita@gmail.com>
2024-02-25 10:44:00 -0500

    ./config: ∋ fa_linkedin_in_→fa_linkedin_

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 import { instagram_url, linkedin_url, patreon_url, youtube_url } from '@btakita/domain--any--brookebrodack/social'
-import { fa_instagram_, fa_linkedin_in_, fa_patreon_, fa_youtube_ } from '@btakita/ui--any--brookebrodack/icon'
+import { fa_instagram_, fa_linkedin_, fa_patreon_, fa_youtube_ } from '@btakita/ui--any--brookebrodack/icon'
 import { type site_T } from '@rappstack/domain--server/site'
 import { type social_T } from '@rappstack/domain--server/social'
 import { sqlite_db__name__set } from '@rappstack/domain--server/sqlite'
@@ -35,7 +35,7 @@ export const social_a1:social_T[] = [
 		active: true
 	},
 	{
-		icon_: fa_linkedin_in_,
+		icon_: fa_linkedin_,
 		href: linkedin_url,
 		link_title: site.title + `'s LinkedIn`,
 		active: true
